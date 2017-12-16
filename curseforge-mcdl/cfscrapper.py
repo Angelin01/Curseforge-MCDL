be4a6cc100551ceb2f8cc879b741e71af211d57e
Angelin01 <angeloborsoiross@hotmail.com>
2017-12-16 01:13:14 -0200

    Made function to check dependencies of a mod
    Parameters: str modName, boolean optionalLibraries. Returns a list of modNames

---
+++ curseforge-mcdl/cfscrapper.py
@@ -0,0 +1,19 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def checkDependencies(modName, optional):
+	try:
+	# Webpage consists of https://minecraft.curseforge.com/projects/modName/relations/dependencies[?filter-related-dependencies=3] (the last part will only grab required libraries)
+		parsed = BeautifulSoup(urlopen("https://minecraft.curseforge.com/projects/" + modName + "/relations/dependencies" + ("?filter-related-dependencies=3" if not optional else "")), 'html.parser')
+	except:
+		print("Mod URL for " + modName + "doesn't exist")
+		return([])
+	
+	dependencyList = []
+	
+	# Download links for dependencies are found in divs with classes 'name-wrapper'
+	dependencies = parsed.find_all("div", class_="name-wrapper")
+	for item in dependencies:
+		dependencyList.append(item.find("a")["href"].rsplit('/', 1)[-1]) # Element href inside the a, take the contents after the last slash
+			
+	return(dependencyList)
